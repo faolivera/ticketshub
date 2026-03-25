4a8e92e4d576a1a4b6e9f2e96def3bedbace5aa2
Facundo Olivera <facundo@paws-n-walks.com>
2026-03-25 16:49:40 +0100

    fix(subscriptions): import UsersModule for OptionalJwtAuthGuard dependency

--- backend/src/modules/subscriptions/subscriptions.module.ts
+++ backend/src/modules/subscriptions/subscriptions.module.ts
@@ -4,9 +4,10 @@ import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsRepository } from './subscriptions.repository';
 import { SUBSCRIPTIONS_REPOSITORY } from './subscriptions.repository.interface';
 import { EventsModule } from '../events/events.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, UsersModule],
   controllers: [SubscriptionsController],
   providers: [
     SubscriptionsService,
